2f9077ad5a7dbc9d5d858bbad3049734d438f257
Piotr Maćkowski <gate0300@interia.pl>
2019-02-16 15:13:42 +0100

    Add final template

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,9 +3,9 @@ module ProjectsHelper
 	def join_button
 		if @project.user != current_user
 			if !@is_invited
-				link_to "Dołącz", project_join_path(@project), class: "btn btn-success", method: :post
+				link_to "Dołącz", project_join_path(@project), class: "btn btn-success btn-join", method: :post
 			else
-				link_to "Opuść", project_join_path(@project), class: "btn btn-danger", method: :post
+				link_to "Opuść", project_join_path(@project), class: "btn btn-danger btn-join", method: :post
 			end	 
 		end
 	end
