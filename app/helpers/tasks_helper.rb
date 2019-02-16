a0b94f67880dc5a4e33f81a7a36613de6007e78f
Piotr Maćkowski <gate0300@interia.pl>
2019-02-16 12:58:47 +0100

    Add task label and task button state

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,19 @@
 module TasksHelper
+
+	def done_button(task)
+		if task.done
+			link_to "Powrót do zadania", project_task_done_path(task.project, task), class: "btn btn-warning", method: :patch
+		else
+			link_to "Zakończ zadanie", project_task_done_path(task.project, task), class: "btn btn-success", method: :patch
+		end	 
+	end
+
+	def done_label(task)
+		if task.done
+			content_tag(:span, "Zakończone", class: "label label-success")
+		else
+			content_tag(:span, "W toku", class: "label label-warning")		
+		end	 
+	end
+
 end
